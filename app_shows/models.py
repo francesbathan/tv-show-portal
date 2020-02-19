400253b4440fb0643d8992b7823830b4426f9a5b
frances <frncsbthn@gmail.com>
2020-02-18 16:11:18 -0800

    editable tv show portal created with django

---
+++ app_shows/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+class ShowManager(models.Manager): #validation for show entered
+    def show_validator(self, post_data):
+        errors = {}
+        if len(post_data['show_title']) < 1:
+            errors['show_title'] = 'Please enter a title.'
+        all_title = Show.objects.filter(title=post_data['show_title'])
+        if len(all_title) > 0:
+            errors['duplicate_title'] = "Oh no! That show is already in the portal."
+        if len(post_data['show_network']) < 1:
+            errors['show_network'] = 'Please enter a network.'
+        if len(post_data['show_release_date']) < 1:
+            errors['show_release_date'] = 'Please enter a release date.'
+        if len(post_data['show_desc']) < 1:
+            errors['show_desc'] = 'Please enter a description.'
+        return errors
+
+class Show(models.Model): #show database
+    title = models.CharField(max_length = 255)
+    network = models.CharField(max_length = 255)
+    release_date = models.DateField()
+    desc = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    objects = ShowManager()
---
+++ app_shows/views.py
@@ -0,0 +1,53 @@
+from django.shortcuts import render, redirect
+from .models import *
+from django.contrib import messages
+
+def index(request): #homepage
+    return render(request, 'index.html')
+
+def shows(request): #table with tv show list
+    context = {
+        "shows": Show.objects.all()
+    }
+    return render(request, 'shows.html', context)
+
+def shows_new(request): #form
+    return render(request, 'shows_new.html')
+
+def shows_create(request): #process for form + error validation for unfilled fields
+    form = request.POST
+    all_errors = Show.objects.show_validator(form)
+    print(all_errors)
+    if len(all_errors) > 0:
+        for form_error in all_errors.values():
+            messages.error(request, form_error)
+        return redirect('/shows/new')
+    show=Show.objects.create(title=form['show_title'], network=form['show_network'], release_date=form['show_release_date'], desc=form['show_desc'])
+    return redirect(f'/shows/{show.id}')
+
+def show_info(request, id): #information regarding a show (also destination for newly added show from form)
+    context = {
+        "show": Show.objects.get(id=id)
+    }
+    return render(request, 'show_info.html', context)
+
+def show_edit(request, id):
+    context = {
+        "show": Show.objects.get(id=id)
+    }
+    return render(request, 'show_edit.html', context)
+
+def show_update(request, id):
+    form = request.POST
+    show=Show.objects.get(id=id)
+    show.title=form['show_title']
+    show.network=form['show_network']
+    show.release_date=form['show_release_date']
+    show.desc=form['show_desc']
+    show.save()
+    return redirect(f'/shows/{show.id}')
+
+def destroy(request, id):
+    delete_show = Show.objects.get(id=id)
+    delete_show.delete()
+    return redirect('/shows')
